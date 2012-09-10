e8b97120285a3f987458604a09f9b2f6abc9d1bf
Andrew Savory <git@andrewsavory.com>
2012-09-10 21:48:30 +0100

    Tutorial 3

---
+++ emit_log.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import pika, sys
+
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+
+channel.exchange_declare(exchange='logs',
+						 type='fanout')
+
+message = ' '.join(sys.argv[1:]) or "info: Hello World!"
+
+channel.basic_publish(exchange='logs',
+                      routing_key='',
+                      body=message,
+                      )
+print " [x] Sent %r" % (message,)
+
+connection.close()
