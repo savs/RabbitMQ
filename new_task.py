db76a9ed422594e224189e2da24e076c885dc139
Andrew Savory <git@andrewsavory.com>
2012-09-10 13:21:45 +0100

    Tutorial 2

--- new_task.py
+++ new_task.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python
-import pika
+import sys, pika
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(
-               'localhost'))
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
-channel.queue_declare(queue='hello')
+channel.queue_declare(queue='task_queue', durable=True)
+
+message = ' '.join(sys.argv[1:]) or "Hello World!"
+
 channel.basic_publish(exchange='',
-                      routing_key='hello',
-                      body='Hello World!')
-print " [x] Sent 'Hello World!'"
+                      routing_key='task_queue',
+                      body=message,
+                      properties=pika.BasicProperties(
+                      	delivery_mode = 2,
+                      ))
+print " [x] Sent %r" % (message,)
+
 connection.close()
--- worker.py
+++ worker.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python
-import pika
+import time, pika
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(
-               'localhost'))
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
-channel.queue_declare(queue='hello')
+channel.queue_declare(queue='task_queue', durable=True)
 
 print ' [*] Waiting for messages. To exit press CTRL+C'
 
 def callback(ch, method, properties, body):
-    print " [x] Received %r" % (body,)
+	print " [x] Received %r" % (body,)
+	time.sleep( body.count('.') )
+	print " [x] Done"
+	ch.basic_ack(delivery_tag = method.delivery_tag)
 
+channel.basic_qos(prefetch_count=1)
 channel.basic_consume(callback,
-                      queue='hello',
-                      no_ack=True)
+                      queue='task_queue')
 
 channel.start_consuming()
